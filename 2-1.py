6880eb07639f6e8846ef9897e9768f094d0e3a7b
wdostert <wdostert@live.com>
2023-09-03 16:09:53 -0400

    Exercise 2

--- 2-1.py
+++ 2-1.py
@@ -1,4 +1,6 @@
 #Will Dostert
+
+#Exercise 1 function
 def get_unique_list(input_list):
     unique_list = []
     for item in input_list:
@@ -7,6 +9,24 @@ def get_unique_list(input_list):
     return unique_list
 
 
+#Exercise 2 function
+def get_combined_dict(dict1, dict2):
+    combined_dict = {}
+    for key in dict1:
+        if key in dict2:
+            combined_dict[key] = dict1[key] + dict2[key]
+    return combined_dict
+
+
+
+#Exercise 1 Test
 my_list = [1, 2, 3, 2, 1, 4]
 unique_list = get_unique_list(my_list)
 print(unique_list)
+
+
+
+#Exercise 2 Test
+my_dict_1 = {'a': 5, 'b': 12, 'c': 3, 'd': 9}
+my_dict_2 = {'b': 4, 'c': 9, 'd': 10, 'e': 16}
+combined_dict = get_co
