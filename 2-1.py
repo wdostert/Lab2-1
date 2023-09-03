2356ce8eb1002958396777e56d5c0dcf96b02687
wdostert <wdostert@live.com>
2023-09-03 16:12:56 -0400

    Exercise 2 Fixed

--- 2-1.py
+++ 2-1.py
@@ -29,4 +29,5 @@ print(unique_list)
 #Exercise 2 Test
 my_dict_1 = {'a': 5, 'b': 12, 'c': 3, 'd': 9}
 my_dict_2 = {'b': 4, 'c': 9, 'd': 10, 'e': 16}
-combined_dict = get_co
+combined_dict = get_combined_dict(my_dict_1, my_dict_2)
+print(combined_dict)
